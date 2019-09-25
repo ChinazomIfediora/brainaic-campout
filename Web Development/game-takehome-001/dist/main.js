7e074622a5b4509cd74fa9e31bf63225a2c216ee
spaceofmiah <geek_200@outlook.com>
2019-09-25 14:35:12 +0100

    Create a moving slider

--- Web Development/game-takehome-001/dist/main.js
+++ Web Development/game-takehome-001/dist/main.js
@@ -10,23 +10,37 @@ var ctx = document.querySelector('canvas').getContext('2d');
 var GAME_FRAME = {
 	width: 280,
 	height: 560,
+
 	// game area and margins
 	frameLeftMargin: 20,
 	frameTopMargin: 20,
-	frameRightMargin: 270,
-	frameBottomMargin: 520,
+	frameRightMargin: 264,
+	frameBottomMargin: 535,
 
 	drawGameArea: function drawGameArea(context) {
 		context.strokeStyle = '#6c88ee';
-		context.strokeRect(10, 10, 280, 560);
-
-		context.fillRect(14, 14, 270, 520);
+		context.strokeRect(10, 10, this.width, this.height);
 	},
 
 	clearGameArea: function clearGameArea(context) {
-		context.clearRect(this.frameLeftMargin - 5, this.frameTopMargin - 5, this.frameRightMargin + 5, this.frameBottomMargin + 5);
+		context.clearRect(this.frameLeftMargin - 5, this.frameTopMargin - 5, this.frameRightMargin + 5, this.frameBottomMargin);
 
 		this.drawGameArea(context);
+	},
+
+	drawSlider: function drawSlider(context) {
+		context.beginPath();
+
+		context.lineWidth = 1;
+		context.strokeStyle = '#9bfbf2';
+		context.rect(Math.floor(this.width / 2), this.frameBottomMargin + 22, 40, 5);
+
+		context.closePath();
+		context.stroke();
+	},
+
+	clearSliderArea: function clearSliderArea(context) {
+		context.clearRect(this.frameLeftMargin - 5, this.frameBottomMargin + 20, this.frameRightMargin + 5, this.height - this.frameBottomMargin - 12);
 	}
 };
 
@@ -47,7 +61,7 @@ var Circle = function () {
 		this.lineWidth = Math.floor(Math.random() * 2) + 1;
 		this.radius = Math.floor(Math.random() * 8) + 1;
 		this.xPosition = this.generateXPosition();
-		this.yPosition = this.FRAME_OBJECT["frameTopMargin"];
+		this.yPosition = this.FRAME_OBJECT['frameTopMargin'];
 
 		// sets the lineWith of the circle to the auto generated lineWidth property
 		this.context.lineWidth = this.lineWidth;
@@ -55,17 +69,17 @@ var Circle = function () {
 		this.validateXPosition();
 
 		/**
-   * 
-   * a circle tends to grow based on the size of its radius. 
+   *
+   * a circle tends to grow based on the size of its radius.
    * To make sure a cirlce doesn't touch the game frame left margin:
    * 	   The auto generated radius will be  added to the horizontal position of the circle
    * To make sure a circle doesn't touch the game frame top margin:
-   * 	   The auto generated radius will be added to the vertical position of the circle 
+   * 	   The auto generated radius will be added to the vertical position of the circle
    * To make sure a circle doesn't touch the game frame right margin:
    * 	   The auto generated will be reduced reduced from the horizontal position of the circle
    */
 
-		if (this.xPosition === this.FRAME_OBJECT["frameRightMargin"]) {
+		if (this.xPosition === this.FRAME_OBJECT['frameRightMargin']) {
 			this.xPosition = this.xPosition - this.radius;
 		} else {
 			this.xPosition = this.xPosition + this.radius;
@@ -82,7 +96,7 @@ var Circle = function () {
 	_createClass(Circle, [{
 		key: 'generateXPosition',
 		value: function generateXPosition() {
-			return Math.floor(Math.random() * this.FRAME_OBJECT["frameRightMargin"]);
+			return Math.floor(Math.random() * this.FRAME_OBJECT['frameRightMargin']);
 		}
 
 		/**
@@ -92,11 +106,11 @@ var Circle = function () {
 	}, {
 		key: 'validateXPosition',
 		value: function validateXPosition() {
-			if (this.xPosition < this.FRAME_OBJECT["frameLeftMargin"]) {
-				this.xPosition = this.FRAME_OBJECT["frameLeftMargin"];
+			if (this.xPosition < this.FRAME_OBJECT['frameLeftMargin']) {
+				this.xPosition = this.FRAME_OBJECT['frameLeftMargin'];
 			}
-			if (this.xPosition > this.FRAME_OBJECT["frameRightMargin"]) {
-				this.xPosition = this.FRAME_OBJECT["frameRightMargin"];
+			if (this.xPosition > this.FRAME_OBJECT['frameRightMargin']) {
+				this.xPosition = this.FRAME_OBJECT['frameRightMargin'];
 			}
 		}
 
@@ -110,12 +124,11 @@ var Circle = function () {
 		key: 'atBottomMargin',
 		value: function atBottomMargin() {
 			var flag = false;
-			if (this.yPosition < this.FRAME_OBJECT["frameBottomMargin"]) {
+			if (this.yPosition < this.FRAME_OBJECT['frameBottomMargin']) {
 				flag = false;
 			}
-			if (this.yPosition >= this.FRAME_OBJECT["frameBottomMargin"]) {
+			if (this.yPosition >= this.FRAME_OBJECT['frameBottomMargin']) {
 				flag = true;
-				console.log(this.getYPosition());
 			}
 			return flag;
 		}
@@ -174,9 +187,7 @@ var Circle = function () {
 	}, {
 		key: 'remove',
 		value: function remove() {
-			console.log("remove -- called");
 			if (this.atBottomMargin() === true) {
-				console.log("at bottom");
 				this.context = null;
 				this.FRAME_OBJECT = null;
 			}
@@ -195,7 +206,7 @@ var Circle = function () {
 				_this.clear(); // clear the game area
 
 				if (_this.atBottomMargin() === true) {
-					// stop the current running interval and remove all circle drawing 
+					// stop the current running interval and remove all circle drawing
 					// tools when circle is at the bottom of game playing area
 					clearInterval(fallInterval);
 					_this.remove();
@@ -212,12 +223,176 @@ var Circle = function () {
 	return Circle;
 }();
 
-// drawing the game area
+var Slider = function () {
+	/**
+  *
+  * @param {String} sColor slider outline color
+  * @param {Object} frameObject game play area
+  * @param {Object} context An object for drawing 2d shapes within a canvas
+  */
+	function Slider(sColor, frameObject, context) {
+		_classCallCheck(this, Slider);
+
+		this.FRAME_OBJECT = frameObject;
+		this.context = context;
+		this.strokeColor = sColor;
+
+		// horizontal & vertical position
+		this.xPos = Math.floor(this.FRAME_OBJECT.frameRightMargin / 2);
+		this.yPos = this.FRAME_OBJECT.frameBottomMargin + 22;
+
+		// width & height
+		this.width = 40;
+		this.height = 5;
+
+		// controls
+		this.controlKeys = ['ArrowLeft', 'ArrowRight'];
+	}
+
+	/**
+  * returns a slider horizontal position
+  */
 
 
-GAME_FRAME.drawGameArea(ctx);
+	_createClass(Slider, [{
+		key: 'getXPosition',
+		value: function getXPosition() {
+			return this.xPos;
+		}
 
-// create a falling circle
-var circle = new Circle('#5c3c8e', GAME_FRAME, ctx);
-circle.draw();
-circle.fallDown();
+		/**
+   *
+   * @param {Event} event checks if an event object whose key property is contained in
+   * control keys
+   */
+
+	}, {
+		key: 'validateKey',
+		value: function validateKey(event) {
+			return this.controlKeys.filter(function (i) {
+				return event['key'] === i;
+			});
+		}
+
+		/**
+   * clear slider game area and redraw slider in it's news horizontal
+   * position while maintaining it's vertical position
+   */
+
+	}, {
+		key: 'draw',
+		value: function draw() {
+			// clear slider area
+			this.FRAME_OBJECT.clearSliderArea(this.context);
+
+			// draw slider
+			this.context.beginPath();
+			this.context.lineWidth = 1;
+			this.context.strokeStyle = this.strokeColor;
+			this.context.rect(this.getXPosition(), this.yPos, this.width, this.height);
+			this.context.closePath();
+
+			this.context.stroke();
+		}
+
+		/**
+   * moves slider right and making sure slider does not go beyound
+   * right margin of game play area
+   */
+
+	}, {
+		key: 'moveRight',
+		value: function moveRight() {
+			if (this.xPos > this.FRAME_OBJECT.frameRightMargin - this.width + 12) {
+				// make the x position same as the right margin of game play area
+				this.xPos = this.FRAME_OBJECT.frameRightMargin - this.width + 12;
+			} else {
+				// increment the width so it wouldn't overlap on game play area
+				this.xPos = this.xPos + 4;
+			}
+		}
+
+		/**
+   * moves slider left and making sure slider does not go beyound
+   * left margin of game play area
+   */
+
+	}, {
+		key: 'moveLeft',
+		value: function moveLeft() {
+			if (this.xPos < this.FRAME_OBJECT.frameLeftMargin) {
+				// make the x position same as the left margin of game play area
+				this.xPos = this.FRAME_OBJECT.frameLeftMargin;
+			} else {
+				// deduct just the width so it wouldn't overlap on game play area
+				this.xPos = this.xPos - 4;
+			}
+		}
+
+		/**
+   *
+   * @param {String} color set slider's stroke color
+   */
+
+	}, {
+		key: 'setStrokeColor',
+		value: function setStrokeColor(color) {
+			this.strokeColor = color;
+		}
+
+		/**
+   * return slider's stroke color
+   */
+
+	}, {
+		key: 'getStrokeColor',
+		value: function getStrokeColor() {
+			return this.strokeColor;
+		}
+
+		/**
+   * controls the movement of a slider
+   * @param {Object} event an event object
+   */
+
+	}, {
+		key: 'move',
+		value: function move(event) {
+			var key = this.validateKey(event);
+
+			switch (key.toString()) {
+				case 'ArrowRight':
+					this.moveRight();
+					this.draw();
+					break;
+
+				case 'ArrowLeft':
+					this.moveLeft();
+					this.draw();
+					break;
+
+				default:
+					break;
+			}
+		}
+	}]);
+
+	return Slider;
+}();
+
+var playGame = function playGame() {
+	GAME_FRAME.drawGameArea(ctx); // drawing the game area
+	var slider = new Slider('#9bfbf2', GAME_FRAME, ctx);
+	slider.draw();
+
+	document.addEventListener('keydown', function (e) {
+		slider.move(e);
+	});
+
+	// create a falling circle
+	var circle = new Circle('#5c3c8e', GAME_FRAME, ctx);
+	circle.draw();
+	circle.fallDown();
+};
+
+playGame();
--- Web Development/game-takehome-001/main.js
+++ Web Development/game-takehome-001/main.js
@@ -4,26 +4,52 @@ const ctx = document.querySelector('canvas').getContext('2d');
 const GAME_FRAME = {
 	width: 280,
 	height: 560,
+
 	// game area and margins
 	frameLeftMargin: 20,
 	frameTopMargin: 20,
-	frameRightMargin: 270,
-	frameBottomMargin: 520,
+	frameRightMargin: 264,
+	frameBottomMargin: 535,
 
-	drawGameArea: function (context) {
+	drawGameArea: function(context) {
 		context.strokeStyle = '#6c88ee';
-		context.strokeRect(10, 10, 280, 560);
-
-		context.fillRect(14, 14, 270, 520);
+		context.strokeRect(10, 10, this.width, this.height);
 	},
 
-	clearGameArea : function (context) {
+	clearGameArea: function(context) {
 		context.clearRect(
-			this.frameLeftMargin - 5, this.frameTopMargin - 5, 
-			this.frameRightMargin + 5, this.frameBottomMargin + 5,
+			this.frameLeftMargin - 5,
+			this.frameTopMargin - 5,
+			this.frameRightMargin + 5,
+			this.frameBottomMargin
 		);
 
 		this.drawGameArea(context);
+	},
+
+	drawSlider: function(context) {
+		context.beginPath();
+
+		context.lineWidth = 1;
+		context.strokeStyle = '#9bfbf2';
+		context.rect(
+			Math.floor(this.width / 2),
+			this.frameBottomMargin + 22,
+			40,
+			5
+		);
+
+		context.closePath();
+		context.stroke();
+	},
+
+	clearSliderArea: function(context) {
+		context.clearRect(
+			this.frameLeftMargin - 5,
+			this.frameBottomMargin + 20,
+			this.frameRightMargin + 5,
+			this.height - this.frameBottomMargin - 12
+		);
 	}
 };
 
@@ -35,61 +61,56 @@ class Circle {
 	 * @param {Object} context An object for drawing 2d shapes within a canvas
 	 */
 	constructor(sColor, frameObject, context) {
-
-        this.FRAME_OBJECT = frameObject;
-        this.strokeColor = sColor;
+		this.FRAME_OBJECT = frameObject;
+		this.strokeColor = sColor;
 		this.context = context;
 
 		this.lineWidth = Math.floor(Math.random() * 2) + 1;
 		this.radius = Math.floor(Math.random() * 8) + 1;
 		this.xPosition = this.generateXPosition();
-		this.yPosition = this.FRAME_OBJECT["frameTopMargin"];
-
+		this.yPosition = this.FRAME_OBJECT['frameTopMargin'];
 
 		// sets the lineWith of the circle to the auto generated lineWidth property
 		this.context.lineWidth = this.lineWidth;
-	
+
 		this.validateXPosition();
 
-
 		/**
-		 * 
-		 * a circle tends to grow based on the size of its radius. 
+		 *
+		 * a circle tends to grow based on the size of its radius.
 		 * To make sure a cirlce doesn't touch the game frame left margin:
 		 * 	   The auto generated radius will be  added to the horizontal position of the circle
 		 * To make sure a circle doesn't touch the game frame top margin:
-		 * 	   The auto generated radius will be added to the vertical position of the circle 
+		 * 	   The auto generated radius will be added to the vertical position of the circle
 		 * To make sure a circle doesn't touch the game frame right margin:
 		 * 	   The auto generated will be reduced reduced from the horizontal position of the circle
 		 */
 
-		if (this.xPosition === this.FRAME_OBJECT["frameRightMargin"]){
-			this.xPosition = this.xPosition - this.radius;	
-		}
-		else {
+		if (this.xPosition === this.FRAME_OBJECT['frameRightMargin']) {
+			this.xPosition = this.xPosition - this.radius;
+		} else {
 			this.xPosition = this.xPosition + this.radius;
 		}
 
 		this.yPosition = this.yPosition + this.radius;
-
 	}
 
 	/**
 	 * generates an horizontal position for a cirlce
 	 */
-	generateXPosition(){
-		return Math.floor(Math.random() * this.FRAME_OBJECT["frameRightMargin"]);
+	generateXPosition() {
+		return Math.floor(Math.random() * this.FRAME_OBJECT['frameRightMargin']);
 	}
 
 	/**
 	 * validate that the horinzontal position of a circle is within the game area.
 	 */
 	validateXPosition() {
-		if(this.xPosition < this.FRAME_OBJECT["frameLeftMargin"]){
-			this.xPosition = this.FRAME_OBJECT["frameLeftMargin"];
+		if (this.xPosition < this.FRAME_OBJECT['frameLeftMargin']) {
+			this.xPosition = this.FRAME_OBJECT['frameLeftMargin'];
 		}
-		if(this.xPosition > this.FRAME_OBJECT["frameRightMargin"]){
-			this.xPosition = this.FRAME_OBJECT["frameRightMargin"];
+		if (this.xPosition > this.FRAME_OBJECT['frameRightMargin']) {
+			this.xPosition = this.FRAME_OBJECT['frameRightMargin'];
 		}
 	}
 
@@ -100,12 +121,11 @@ class Circle {
 	 */
 	atBottomMargin() {
 		let flag = false;
-		if (this.yPosition < this.FRAME_OBJECT["frameBottomMargin"]){
+		if (this.yPosition < this.FRAME_OBJECT['frameBottomMargin']) {
 			flag = false;
 		}
-		if (this.yPosition >= this.FRAME_OBJECT["frameBottomMargin"]){
+		if (this.yPosition >= this.FRAME_OBJECT['frameBottomMargin']) {
 			flag = true;
-			console.log(this.getYPosition());
 		}
 		return flag;
 	}
@@ -113,14 +133,14 @@ class Circle {
 	/**
 	 * returns the horizontal position of a circle
 	 */
-	getXPosition(){
+	getXPosition() {
 		return this.xPosition;
 	}
 
 	/**
 	 * increase the vertical position of a circle
 	 */
-	updateYPosition(){
+	updateYPosition() {
 		this.yPosition = this.yPosition + 8;
 	}
 
@@ -132,12 +152,10 @@ class Circle {
 		return this.yPosition;
 	}
 
-
 	clear() {
 		// clear drawing area
 		this.FRAME_OBJECT.clearGameArea(this.context);
 	}
-
 
 	/**
 	 * draws a circle on the on the game play area
@@ -145,19 +163,14 @@ class Circle {
 	draw() {
 		this.context.beginPath();
 
-		this.context.arc(
-			this.xPosition, this.yPosition, this.radius, 0, 20, false
-		);
-		
+		this.context.arc(this.xPosition, this.yPosition, this.radius, 0, 20, false);
+
 		this.context.closePath();
 		this.context.stroke();
 	}
 
-
 	remove() {
-		console.log("remove -- called");
-		if (this.atBottomMargin() === true){
-			console.log("at bottom")
+		if (this.atBottomMargin() === true) {
 			this.context = null;
 			this.FRAME_OBJECT = null;
 		}
@@ -168,29 +181,165 @@ class Circle {
 	 */
 	fallDown() {
 		let fallInterval = setInterval(() => {
-			this.clear();			// clear the game area
+			this.clear(); // clear the game area
 
-			if (this.atBottomMargin() === true){
-				// stop the current running interval and remove all circle drawing 
+			if (this.atBottomMargin() === true) {
+				// stop the current running interval and remove all circle drawing
 				// tools when circle is at the bottom of game playing area
 				clearInterval(fallInterval);
-				this.remove();			
+				this.remove();
 			} else {
 				// keep updating circle's veritcal position and keep drawing a circle
 				// until the circle gets to the bottom of game playing area
 				this.updateYPosition();
 				this.draw();
 			}
-
 		}, 300);
 	}
 }
 
+class Slider {
+	/**
+	 *
+	 * @param {String} sColor slider outline color
+	 * @param {Object} frameObject game play area
+	 * @param {Object} context An object for drawing 2d shapes within a canvas
+	 */
+	constructor(sColor, frameObject, context) {
+		this.FRAME_OBJECT = frameObject;
+		this.context = context;
+		this.strokeColor = sColor;
 
-// drawing the game area
-GAME_FRAME.drawGameArea(ctx);
+		// horizontal & vertical position
+		this.xPos = Math.floor(this.FRAME_OBJECT.frameRightMargin / 2);
+		this.yPos = this.FRAME_OBJECT.frameBottomMargin + 22;
 
-// create a falling circle
-const circle = new Circle('#5c3c8e', GAME_FRAME, ctx);
-circle.draw();
-circle.fallDown();
+		// width & height
+		this.width = 40;
+		this.height = 5;
+
+		// controls
+		this.controlKeys = ['ArrowLeft', 'ArrowRight'];
+	}
+
+	/**
+	 * returns a slider horizontal position
+	 */
+	getXPosition() {
+		return this.xPos;
+	}
+
+	/**
+	 *
+	 * @param {Event} event checks if an event object whose key property is contained in
+	 * control keys
+	 */
+	validateKey(event) {
+		return this.controlKeys.filter(i => event['key'] === i);
+	}
+
+	/**
+	 * clear slider game area and redraw slider in it's news horizontal
+	 * position while maintaining it's vertical position
+	 */
+	draw() {
+		// clear slider area
+		this.FRAME_OBJECT.clearSliderArea(this.context);
+
+		// draw slider
+		this.context.beginPath();
+		this.context.lineWidth = 1;
+		this.context.strokeStyle = this.strokeColor;
+		this.context.rect(this.getXPosition(), this.yPos, this.width, this.height);
+		this.context.closePath();
+
+		this.context.stroke();
+	}
+
+	/**
+	 * moves slider right and making sure slider does not go beyound
+	 * right margin of game play area
+	 */
+	moveRight() {
+		if (this.xPos > this.FRAME_OBJECT.frameRightMargin - this.width + 12) {
+			// make the x position same as the right margin of game play area
+			this.xPos = this.FRAME_OBJECT.frameRightMargin - this.width + 12;
+		} else {
+			// increment the width so it wouldn't overlap on game play area
+			this.xPos = this.xPos + 4;
+		}
+	}
+
+	/**
+	 * moves slider left and making sure slider does not go beyound
+	 * left margin of game play area
+	 */
+	moveLeft() {
+		if (this.xPos < this.FRAME_OBJECT.frameLeftMargin) {
+			// make the x position same as the left margin of game play area
+			this.xPos = this.FRAME_OBJECT.frameLeftMargin;
+		} else {
+			// deduct just the width so it wouldn't overlap on game play area
+			this.xPos = this.xPos - 4;
+		}
+	}
+
+	/**
+	 *
+	 * @param {String} color set slider's stroke color
+	 */
+	setStrokeColor(color) {
+		this.strokeColor = color;
+	}
+
+	/**
+	 * return slider's stroke color
+	 */
+	getStrokeColor() {
+		return this.strokeColor;
+	}
+
+	/**
+	 * controls the movement of a slider
+	 * @param {Object} event an event object
+	 */
+	move(event) {
+		let key = this.validateKey(event);
+
+		switch (key.toString()) {
+			case 'ArrowRight':
+				this.moveRight();
+				this.draw();
+				break;
+
+			case 'ArrowLeft':
+				this.moveLeft();
+				this.draw();
+				break;
+
+			default:
+				break;
+		}
+	}
+}
+
+
+
+const playGame = ( ) => {
+	GAME_FRAME.drawGameArea(ctx); // drawing the game area
+	const slider = new Slider('#9bfbf2', GAME_FRAME, ctx);
+	slider.draw();
+
+	document.addEventListener('keydown', e => {
+		slider.move(e);
+	});
+
+	// create a falling circle
+	const circle = new Circle('#5c3c8e', GAME_FRAME, ctx);
+	circle.draw();
+	circle.fallDown();
+}
+
+
+
+playGame();
